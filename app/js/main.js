beb3ab44e47a4f1e69118211108e8c4a854cf9fa
Toan Nguyen <toan.van.nguyen@opendigital.asia>
2018-05-15 15:48:28 +0700

    setup global

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,11 @@ window.onload = function () {
 function initAppItem() {
   let init = 'init app';
   console.log(init);
+  hoverNav();
+}
+
+function hoverNav() {
+	var navigation = document.querySelector('.navigation-top');
+	var navItem = navigation.querySelectorAll('.menu-parent > li');
+	console.log(navItem);
 }
