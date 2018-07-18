810c4ac9641957916f019137cc2cd5bfbb251f8b
Thuy Tran Phuong <thuy.tran@adaptis.io>
2018-07-18 13:27:16 +0700

    done push utm to form

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function() {
 	showModal();
 	buttonCloseModal();
+
+	var form = document.querySelector('.form-register');
+	form.addEventListener('submit', function(e) {
+		submitForm(e.target);
+		e.preventDefault();
+	}, false);
 });
 
 // Click button to show modal
@@ -11,7 +17,6 @@ function showModal() {
 			e.preventDefault();
 			var modalTarget = $(this).data('target-modal');
 			var package = $(this).data('option');
-			console.log(package);
 
 			if (modalTarget.length > 0) {
 				$(modalTarget).addClass('show');
@@ -44,4 +49,58 @@ function changeValueOptionInForm(option) {
 	if (select.length > 0) {
 		option ? $(select).val(option) : $(select).val('');
 	}
+}
+
+function parse_query_string(query) {
+  var vars = query.split("&");
+  var query_string = {};
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    var key = decodeURIComponent(pair[0]);
+    var value = decodeURIComponent(pair[1]);
+
+    if (typeof query_string[key] === "undefined") {
+      query_string[key] = decodeURIComponent(value);
+
+    } else if (typeof query_string[key] === "string") {
+      var arr = [query_string[key], decodeURIComponent(value)];
+      query_string[key] = arr;
+    } else {
+      query_string[key].push(decodeURIComponent(value));
+    }
+  }
+  return query_string;
+}
+
+
+function submitForm(form) {
+	var url = "https://script.google.com/macros/s/AKfycbxv-REiX_H446-KAwlMuIfzEjGyBMdm6sYcMp0P/exec";
+	var url_location = window.location.href;
+	var params = window.location.search.replace('?', '');
+	var paramsList = parse_query_string(params);
+	var formData = new FormData(form);
+	var xhr = new XMLHttpRequest();
+
+	formData.append('url', url_location);
+
+	for (var key in paramsList) {
+		formData.append(key, paramsList[key]);
+	}
+
+	// xhr.onprogress = function(pe) {
+	// 	console.log('on progress');
+	// }
+
+	xhr.onload = function(e) {
+		if (xhr.status == 200) {
+			window.location.href="/thankyou";
+		} else {
+			alert('Error');
+		}
+	}
+
+	xhr.open('POST', url, true);
+
+	xhr.send(formData);
+	return false;
 }
